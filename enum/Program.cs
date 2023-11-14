858660645989747773e10c7a3f64e6bc4f5300b1
Leyla-Mammad <leylamamedova19960@gmail.com>
2023-11-15 01:42:06 +0400

    list

---
+++ enum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> list = new List<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
+
+        int sum = 0;
+        foreach (var item in list)
+        {
+            sum += item;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
